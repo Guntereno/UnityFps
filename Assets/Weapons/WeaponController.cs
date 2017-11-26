3233ac8536e12030b2a0c817eca575f6dcbe0c3f
Guntereno <GunterPete@hotmail.com>
2017-11-26 11:28:53 +0000

    Added gun firing animations.

---
+++ Assets/Weapons/WeaponController.cs
@@ -0,0 +1,18 @@
+﻿using InControl;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    [SerializeField]
+    Animator m_animator;
+
+
+    static readonly int kParamTriggerAmount = Animator.StringToHash("TriggerAmount");
+
+	void Update ()
+    {
+        InputDevice device = InputManager.ActiveDevice;
+        float triggerAmount = device.RightTrigger.Value;
+        m_animator.SetFloat(kParamTriggerAmount, triggerAmount);
+    }
+}
